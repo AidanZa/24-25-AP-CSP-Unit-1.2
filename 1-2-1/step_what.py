744dc3e62c44db0d4c18ef9178b0e8abfcd2e90f
12038 <aidan.zachowski@vtsd.com>
2024-10-31 11:45:32 -0400

    updated

--- 1-2-1/step_what.py
+++ 1-2-1/step_what.py
@@ -2,18 +2,26 @@
 #-----import statements-----
 import turtle as trtl
 import random as rand
-score=0
 
 #-----game configuration----
 spot_color = "red"
 spot_size = 2
 spot_shape = "circle"
+score=0
+
+def update_score_for_spot(x,y):
+    global score
+    score += 1
+    score1.clear()
+    score1.write(score, font=font_setup)
 
 #-----initialize turtle-----
 spot = trtl.Turtle()
 spot.shape(spot_shape)
 spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
+score1 = trtl.Turtle()
+font_setup = "Arial", 20, "normal"
 
 #-----game functions--------
 def change_position():
@@ -21,18 +29,15 @@ def change_position():
     new_xpos=rand.randint(-400,400)
     new_ypos=rand.randint(-300,300)
     spot.goto(new_xpos,new_ypos)
-    update_score_for_spot(new_xpos,new_ypos)
+
+score1.penup()
+score1.goto(-400,330)
 
 def spot_clicked(x,y):
     change_position()
-
-def update_score_for_spot(x,y):
-    global score
-    score += 1
-    print(score)
+    update_score_for_spot(x,y)
 
 #-----events----------------
-spot.onclick(spot_clicked)
-spot.onclick(update_score_for_spot)
+spot.onclick(spot_clicked),(update_score_for_spot)
 wn = trtl.Screen()
 wn.mainloop()
