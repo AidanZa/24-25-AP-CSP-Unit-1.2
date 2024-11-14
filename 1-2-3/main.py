2cc718127164987ff67178188120ecbad8c0b740
12038 <aidan.zachowski@vtsd.com>
2024-11-14 13:51:32 -0500

    updated

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -7,7 +7,7 @@ apple_image = "apple.gif" # Store the file name of your shape
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
-
+wn.bgpic("background.gif")
 apple = trtl.Turtle()
 
 #-----functions-----
@@ -16,8 +16,13 @@ def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
+def apple_down(active_apple):
+  x_coor = active_apple.xcor()
+  y_coor = active_apple.ycor()
+  active_apple.goto(x_coor, y_coor - 300)
 
 #-----function calls-----
 draw_apple(apple)
+apple_down(apple)
 
 wn.mainloop()
